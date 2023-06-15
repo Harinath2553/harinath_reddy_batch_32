769e8d4719648f2eb8fc35608790c360bc23f03e
Harinath2553 <119629034+Harinath2553@users.noreply.github.com>
2023-06-15 00:37:41 -0500

    Delete Backend/Asgn 06-08-23/src/June 13 directory

--- Backend/Asgn 06-08-23/src/June 13/src/InterThread.java
+++
@@ -1,8 +0,0 @@
-public class InterThread implements Runnable{
-
-    public void run(){
-        for(int i=0;i<=10;i++){
-            System.out.println("Thread 2 "+i);
-        }
-    }
-}
--- Backend/Asgn 06-08-23/src/June 13/src/Main.java
+++
@@ -1,9 +0,0 @@
-
-public class Main extends extThread{
-    public static void main(String [] args){
-        Thread t=new extThread();
-        Thread t1=new Thread(new InterThread());
-        t.start();
-        t1.start();
-    }
-}
