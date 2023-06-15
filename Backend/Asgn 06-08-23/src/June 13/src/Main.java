a581117d2f1de9c5f0eab0fc1340e57aff6a830e
Harinath2553 <119629034+Harinath2553@users.noreply.github.com>
2023-06-15 00:32:48 -0500

    Add files via upload

---
+++ Backend/Asgn 06-08-23/src/June 13/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main extends extThread{
+    public static void main(String [] args){
+        Thread t=new extThread();
+        Thread t1=new Thread(new InterThread());
+        t.start();
+        t1.start();
+    }
+}
